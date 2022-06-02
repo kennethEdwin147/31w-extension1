a0f2d54f6ef7af9ff94318a3f7afe0b3d43a77ed
Gbeti <e2194460@cmaisonneuve.qc.ca>
2022-06-02 12:18:26 -0400

    s3-c2 Suite configuration de notre premier plugin

--- ex1_31w.php
+++ ex1_31w.php
@@ -5,6 +5,20 @@
  * Description: Une première extension pour l'exercice 1
  *Author URI: https://github.com/kennethEdwin147
  */
- 
 ?>
-    
+<?php 
+
+function fonction_generalle() {
+    function mon_31w_enqueue() {
+        wp_enqueue_style(	'mon_31w-style', 
+                            get_stylesheet_uri(), 
+                            array(),
+                             _S_VERSION );
+    }
+    add_action( 'wp_enqueue_scripts', 'mon_31w_enqueue' );
+}
+
+add_action('init', "fonction_generalle");
+
+
+?>
