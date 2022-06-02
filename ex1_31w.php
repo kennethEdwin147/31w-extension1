5a0ee5678b9a6fe2a521e6ac00da9d6aeae44259
Gbeti <e2194460@cmaisonneuve.qc.ca>
2022-06-02 11:42:39 -0400

    s3-c2 configuration de notre premier plugin

---
+++ ex1_31w.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Extension de l'exercice #1
+ * Author: kenneth Gbeti
+ * Description: Une première extension pour l'exercice 1
+ *Author URI: https://github.com/kennethEdwin147
+ */
+ 
+?>
+    
