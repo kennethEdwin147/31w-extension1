b7c11557cf2a17171ad7ecb8650c3a2e3f44dd0e
kennethEdwin147 <edwingbeti@gmail.com>
2022-06-10 00:23:59 -0400

    Ajout du lien github-page dans readme.md

--- ex1_31w.php
+++ ex1_31w.php
@@ -5,20 +5,15 @@
  * Description: Une nouvelle extension pour l'exercice 1
  *Author URI: https://github.com/kennethEdwin147/31w-extension1
  */
-?>
-<?php 
 
 function fonction_generalle() {
     function mon_31w_enqueue() {
         wp_enqueue_style(	'mon_31w-style', 
                             get_stylesheet_uri(), 
                             array(),
-                             _S_VERSION );
+                            filemtime(get_template_directory() . '/style.css'));
     }
     add_action( 'wp_enqueue_scripts', 'mon_31w_enqueue' );
 }
 
 add_action('init', "fonction_generalle");
-
-
-?>
--- readme.md
+++ readme.md
@@ -2,11 +2,13 @@
 ## Première extension Wordpress
 ### Contient 2 fichiers
 
+[Github-page](https://kennethedwin147.github.io/31w/)
+
 > L'extension contient 3 fichiers:
 index.php
 ex1_31w.php
 readme.md
 
 
-<!-- lien vers gitgub page -->
-[]()
+Pour plus d'information sur la conception de thème
+[wp developper guide](https://developer.wordpress.org/)
